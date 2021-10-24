395e8f42834fc27280aeaaa211278bc6fbd09c8f
lv <~@l1v.in>
2021-10-24 12:50:25 -0400

    impr: Make formatting consistent and add debug to unix script

--- scripts/heartbeat-client-unix.sh
+++ scripts/heartbeat-client-unix.sh
@@ -8,22 +8,30 @@ fi
 # shellcheck disable=SC1091
 . "$HOME/.env"
 
-if [ -z "$(which xprintidle)" ]; then
-  echo "xprintidle not found, please install it!"
-  exit 1
-fi
+if [ -z "$HEARTBEAT_AUTH" ] || [ -z "$HEARTBEAT_DEVICE_NAME" ] || [ -z "$HEARTBEAT_HOSTNAME" ]; then
+    echo "Environment variables not setup correctly!"
+    echo "HEARTBEAT_AUTH: $HEARTBEAT_AUTH"
+    echo "HEARTBEAT_DEVICE_NAME: $HEARTBEAT_HOSTNAME"
+    echo "HEARTBEAT_HOSTNAME: $HEARTBEAT_HOSTNAME"
+    exit 1
+else
+    if [ -z "$(which xprintidle)" ]; then
+        echo "xprintidle not found, please install it!"
+        exit 1
+    fi
 
-# Check if kscreenlocker is running. Only works on KDE
-SCREEN_LOCKED="$(pgrep kscreenlocker)"
-# Check when the last keyboard or mouse event was sent
-LAST_INPUT_MS="$(xprintidle)"
+    # Check if kscreenlocker is running. Only works on KDE
+    SCREEN_LOCKED="$(pgrep kscreenlocker)"
+    # Check when the last keyboard or mouse event was sent
+    LAST_INPUT_MS="$(xprintidle)"
 
-# Make sure the device was used in the last 2 minutes
-# and make sure screen is unlocked
-if [ "$LAST_INPUT_MS" -lt 120000 ] && [ -z "$SCREEN_LOCKED" ]; then
-  {
-    echo "$(date +"%Y/%m/%d %T") - Running Heartbeat"
-    curl -s -X POST -H "Auth: $HEARTBEAT_AUTH" -H "Device: $HEARTBEAT_DEVICE_NAME" "$HEARTBEAT_HOSTNAME/api/beat"
-    echo ""
-  } >> "$HEARTBEAT_LOG_DIR/heartbeat.log" 2>&1
+    # Make sure the device was used in the last 2 minutes
+    # and make sure screen is unlocked
+    if [ "$LAST_INPUT_MS" -lt 120000 ] && [ -z "$SCREEN_LOCKED" ]; then
+        {
+            echo "$(date +"%Y/%m/%d %T") - Running Heartbeat"
+            curl -s -X POST -H "Auth: $HEARTBEAT_AUTH" -H "Device: $HEARTBEAT_DEVICE_NAME" "$HEARTBEAT_HOSTNAME/api/beat"
+            echo ""
+        } >> "$HEARTBEAT_LOG_DIR/heartbeat.log" 2>&1
+    fi
 fi
