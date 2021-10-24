00195041c341519eb68ce74769d6ab0a0fa96f7e
sourtaste000 <me@sourtaste000.dev>
2021-10-24 10:49:12 -0700

    fix script

--- scripts/heartbeat-client-macOS.sh
+++ scripts/heartbeat-client-macOS.sh
@@ -8,12 +8,12 @@ fi
 # shellcheck disable=SC1091
 . "$HOME/.heartbeat"
 
-if [[ -z "$HEARTBEAT_AUTH" ]] || [[ -z "$HEARTBEAT_DEVICE_NAME" ]] || [[ -z "$HEARTBEAT_HOSTNAME" ]]; then
+if [[ -z "$HEARTBEAT_AUTH" ]] || [[ -z "$HEARTBEAT_LOG_DIR" ]] || [[ -z "$HEARTBEAT_HOSTNAME" ]] || [[ -z "$HEARTBEAT_DEVICE_NAME" ]]; then
     echo "Environment variables not setup correctly!"
     echo "HEARTBEAT_AUTH: $HEARTBEAT_AUTH"
     echo "HEARTBEAT_LOG_DIR: $HEARTBEAT_LOG_DIR"
     echo "HEARTBEAT_HOSTNAME: $HEARTBEAT_HOSTNAME"
-    echo "HEARTBEAT_DEVICE_NAME: $HEARTBEAT_HOSTNAME"
+    echo "HEARTBEAT_DEVICE_NAME: $HEARTBEAT_DEVICE_NAME"
     exit 1
 else
     # Make log dir if it doesn't exist
@@ -21,11 +21,10 @@ else
         mkdir -p "$HEARTBEAT_LOG_DIR" || exit 1
     fi
 
-    # Check when the last HID event was sent
-    LAST_INPUT_MS="$(ioreg -c IOHIDSystem | awk '/HIDIdleTime/{print $NF}')"
+    LAST_INPUT_SEC=$((`ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/ !{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000))
 
     # Launchd will not execute the task if the system is locked or sleeping, so no need to worry about the screen lock state
-    if [[ $LAST_INPUT_MS -lt 120000 ]]; then
+    if [[ $LAST_INPUT_SEC -lt 120 ]]; then
         {
             echo "$(date +"%Y/%m/%d %T") - Running Heartbeat"
             curl -s -X POST -H "Auth: $HEARTBEAT_AUTH" -H "Device: $HEARTBEAT_DEVICE_NAME" "$HEARTBEAT_HOSTNAME/api/beat"
