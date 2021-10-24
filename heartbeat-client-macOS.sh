9c36efb0fa1b1aa43ae268f4e5437334633a2c9f
sourtaste000 <me@sourtaste000.dev>
2021-10-23 20:58:35 -0700

    clarify

--- heartbeat-client-macOS.sh
+++ heartbeat-client-macOS.sh
@@ -14,7 +14,7 @@ if [ -z "$HEARTBEAT_AUTH" ] || [ -z "$HEARTBEAT_DEVICE_NAME" ] || [ -z "$HEARTBE
   echo "HEARTBEAT_HOSTNAME: $HEARTBEAT_HOSTNAME"
   exit
 else
-  # Launchd will not work if the system is locked/in sleep, so do not have to worry about the lock
+  # Launchd will execute task if the system is locked/in sleep, so do not have to worry about the lock
   if [[ $LAST_INPUT_MS -lt 120000 ]]; then
     echo "$(date +"%Y/%m/%d %T") - Running Heartbeat"
     curl -s -X POST -H "Auth: $HEARTBEAT_AUTH" -H "Device: $HEARTBEAT_DEVICE_NAME" "$HEARTBEAT_HOSTNAME/api/beat"
